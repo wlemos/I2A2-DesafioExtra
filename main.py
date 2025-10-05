423665c8481d789758f9292ea47c9eeceb168ff5
wlemos <wagnerlemoscosta@gmail.coms>
2025-10-05 17:53:14 -0300

    ajuste para identificar a memória erro

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import streamlit as st
 from agents.orchestrator import OrchestratorAgent
-from utils import supabase_client
+from utils.supabase_client import supabase
 
 os.environ["GEMINI_API_KEY"] = st.secrets["GEMINI_API_KEY"]
 os.environ["SUPABASE_URL"] = st.secrets["SUPABASE_URL"]
